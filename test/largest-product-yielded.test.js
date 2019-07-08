9e8b15a3117acfe93ccb8cd03f29db22b4ae7e0b
Lili Boxer <ninaliboxer@gmail.com>
2019-07-08 16:38:49 -0700

    2nd test on largest product yielded

--- test/largest-product-yielded.test.js
+++ test/largest-product-yielded.test.js
@@ -6,4 +6,14 @@ describe('largest product yielded', () => {
     const sort = largestProductYielded(arr);
     expect(sort).toEqual([-70, -10, -10, 5, 7, 29, 30]);
   });
+  it('grab 3 highest numbers', () => {
+    const arr = [-70, -10, -10, 5, 7, 29, 30];
+    const highest = largestProductYielded(arr);
+    expect(highest).toEqual([]);
+  });
+
 });
+
+// 2 or 0 neg numbers 
+
+// check if highest negative number is less than highest positive number 
