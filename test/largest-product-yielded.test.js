c7c2baf381548fb2c046bbe9033f9ae8c4eba170
Lili Boxer <ninaliboxer@gmail.com>
2019-07-08 16:15:39 -0700

    sorted array

--- test/largest-product-yielded.test.js
+++ test/largest-product-yielded.test.js
@@ -1,5 +1,9 @@
 const { largestProductYielded } = require('../lib/largest-product-yielded');
 
 describe('largest product yielded', () => {
-  it
+  it('sorts array', () => {
+    const arr = [-10, 7, 29, 30, 5, -10, -70];
+    const sort = largestProductYielded(arr);
+    expect(sort).toEqual([-70, -10, -10, 5, 7, 29, 30]);
+  });
 });
