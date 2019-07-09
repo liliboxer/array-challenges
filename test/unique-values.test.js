58f31494ecc925139c8fca47e696fb7d925589ce
Lili Boxer <ninaliboxer@gmail.com>
2019-07-08 17:35:37 -0700

    unique values testsing working

--- test/unique-values.test.js
+++ test/unique-values.test.js
@@ -1,5 +1,9 @@
-const {uniqueValues} = require('../lib/unique-values');
+const { uniqueValues } = require('../lib/unique-values');
 
 describe('unique values', () => {
-
+  it('remove duplicates', () => {
+    const arr = [3, 3, 5, 3, 12, 7];
+    const unique = uniqueValues(arr);
+    expect(unique).toEqual([3, 5, 12, 7]);
+  });
 });
