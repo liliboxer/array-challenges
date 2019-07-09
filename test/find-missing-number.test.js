d5bc74a9a7d335246561c99b06365cc262be10b6
Lili Boxer <ninaliboxer@gmail.com>
2019-07-08 21:02:40 -0700

    test for finding length of array based on upperBound and lowerBound

--- test/find-missing-number.test.js
+++ test/find-missing-number.test.js
@@ -1,2 +1,18 @@
 const { findMissingNumber } = require('../lib/find-missing-number');
 
+describe('missing number', () => {
+  it('find length of array', () => {
+    const upperBound = 6;
+    const lowerBound = 1;
+    const arrayLength = upperBound - lowerBound;
+    expect(arrayLength).toEqual(5);
+
+  });
+  it('', () => {
+    const arr = [2, 5, 1, 4, 9, 6, 3, 7];
+    const upperBound = 9;
+    const lowerBound = 1;
+    const result = findMissingNumber(arr, upperBound, lowerBound);
+    expect(result).toEqual(8);
+  });
+});
